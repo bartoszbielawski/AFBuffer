2c33250133fc89a618e6f867b7652601c702ceb1
Bartosz Bielawski <bartosz.bielawski@gmail.com>
2018-06-27 00:36:35 +0200

    Added class extending GFXcanvas1 and a copy method

---
+++ afbuffer.h
@@ -0,0 +1,20 @@
+#ifndef AFBUFFER_H
+#define AFBUFFER_H
+
+#include <cstdint>
+
+#include <Adafruit_GFX.h>
+
+class GFXcanvas1Read: public GFXcanvas1
+{
+    public:
+        GFXcanvas1Read(uint16_t w, uint16_t h): GFXcanvas1(w,h) {}
+        bool getPixel(int16_t x, int16_t y);
+};
+
+void copyBitmap(GFXcanvas1Read& src, int16_t sx, int16_t sy,
+                Adafruit_GFX& dst, int16_t dx, int16_t dy,
+                uint16_t w, uint16_t h);
+
+
+#endif //AFBUFFER_H
